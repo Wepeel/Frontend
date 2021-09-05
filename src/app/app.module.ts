d8467acacbc2ae7ec5c37e42c3178ef985c56967
Wepeel <wepeelsuicune@gmail.com>
2021-09-05 22:18:01 +0300

    Create patients component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PatientsComponent } from './patients/patients.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PatientsComponent
   ],
   imports: [
     BrowserModule
